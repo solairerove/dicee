732025116e35e7761bda069b180b5f9342794a0c
Mikita Kryvitski <solairerove@gmail.com>
2019-05-02 18:59:24 +0300

    feature: change indexes to random int in range

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var leftRandomDiceIndex: Int = 0
+    var rightRandomDiceIndex: Int = 0
+
+    @IBOutlet weak var leftDiceImageView: UIImageView!
+    @IBOutlet weak var rightDiceImageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func rollButtomEmitter(_ sender: UIButton) {
+        leftRandomDiceIndex = getRandomIndex(rangeFrom: 0, rangeTo: 5)
+        rightRandomDiceIndex = getRandomIndex(rangeFrom: 0, rangeTo: 5)
+        
+        print(leftRandomDiceIndex, rightRandomDiceIndex)
+    }
 
+    func getRandomIndex(rangeFrom: Int, rangeTo: Int) -> Int {
+        return Int.random(in: rangeFrom ... rangeTo)
+    }
 }
-
